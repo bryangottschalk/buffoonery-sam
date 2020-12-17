55ba3f74a452c96877d40a240a945e635753537a
bryangottschalk <bryanlgottschalk@gmail.com>
2020-12-17 00:20:26 -0500

    functional category put

--- lambda-websocket/category-put/index.js
+++ lambda-websocket/category-put/index.js
@@ -1,8 +1,33 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
+
+const saveCategory = async function (category) {
+  try {
+    await ddb
+      .put({
+        TableName: process.env.TableName,
+        Item: category
+      })
+      .promise();
+    console.log('saveToDDB success');
+  } catch (err) {
+    console.error('saveToDDB error: ', err);
+  }
+};
+
 exports.handler = async (event) => {
-  // TODO implement
+  console.log('ENV VARS:', process.env);
+  console.log('EVENT:', event);
+  const payload = JSON.parse(event.body);
+  console.log('PAYLOAD:', payload);
+  try {
+    await saveCategory(payload);
+  } catch (err) {
+    return err;
+  }
   const response = {
-    statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!')
+    statusCode: 201,
+    body: JSON.stringify('Successfully saved new category.')
   };
   return response;
 };

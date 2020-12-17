2dc6d81dd878e273ebf81b1e89df5ac21caa39ac
bryangottschalk <bryanlgottschalk@gmail.com>
2020-12-16 22:25:45 -0500

    add category/prompt put/deletes funcs to template

--- lambda-websocket/client-connected-get/index.js
+++ lambda-websocket/client-connected-get/index.js
@@ -1,6 +1,6 @@
 const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-08-10' });
-var https = require('https');
+
 const getGameroom = async (key, tablename) => {
   const result = await ddb
     .get({
@@ -13,15 +13,6 @@ const getGameroom = async (key, tablename) => {
 };
 
 exports.handler = async (event, context) => {
-  console.log('context', context);
-  console.log(
-    '🚀 ~ file: index.js ~ line 24 ~ exports.handler= ~ event',
-    event
-  );
-  console.log('process.env', process.env);
-  console.log('table', process.env.TableName);
-  console.log('path', event.pathParameters);
-  console.log('querystr', event.queryStringParameters);
   let roomcode;
   if (event.pathParameters && event.pathParameters.roomcode) {
     roomcode = event.pathParameters.roomcode;
@@ -39,10 +30,7 @@ exports.handler = async (event, context) => {
 
   const response = {
     statusCode: 200,
-    body:
-      gameroom
-        ? JSON.stringify(gameroom)
-        : {},
+    body: gameroom ? JSON.stringify(gameroom) : {},
     headers: {
       'Access-Control-Allow-Headers': '*',
       'Access-Control-Allow-Origin': '*',
@@ -52,62 +40,3 @@ exports.handler = async (event, context) => {
 
   return response;
 };
-
-// OLD CODE
-// function request(options, data) {
-//   return new Promise((resolve, reject) => {
-//     var req = https.request(options, function (res) {
-//       res.setEncoding('utf8');
-//       let responseBody = '';
-//       res.on('data', function (chunk) {
-//         responseBody += chunk;
-//       });
-//       res.on('end', () => {
-//         resolve(JSON.parse(responseBody));
-//       });
-//     }).on('error', function (e) {
-//       reject(e);
-//     });
-
-//     req.write(JSON.stringify(data));
-//     req.end();
-//   });
-
-// }
-
-// if (
-//   gameroom &&
-//   Array.isArray(gameroom.connectedClients) &&
-//   gameroom.connectedClients.length > 0
-// ) {
-//   const postCalls = gameroom.connectedClients.map(async (connectionId) => {
-//     console.log('connectionId:', connectionId);
-//     const objToPost = JSON.stringify({
-//       action: 'sendmessage', data: `CLIENT CONNECTED ${connectionId}`
-//     })
-//     console.log('objToPost', objToPost)
-//     try {
-//       var options = {
-//         host: 'da6wisihu2.execute-api.us-east-1.amazonaws.com',
-//         path: '/dev/@connection',
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//         }
-//       };
-//       await request(options, objToPost)
-
-//     } catch (e) {
-//       if (e.statusCode === 410) {
-//         console.log(`Found stale connection, deleting ${roomcode}`);
-//         await ddb
-//           .delete({ TableName: TABLE_NAME, Key: { roomcode } })
-//           .promise();
-//       } else {
-//         throw e;
-//       }
-//     }
-//   });
-//   console.log('postCalls', postCalls)
-//   await Promise.all(postCalls);
-// }
